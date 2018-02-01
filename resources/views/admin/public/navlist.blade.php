f5058dbf10523665c5b38eb8f022d030f2a06905
chenhuojia <1126089253@qq.com>
2018-02-01 13:45:34 +0800

    f

--- resources/views/admin/public/navlist.blade.php
+++ resources/views/admin/public/navlist.blade.php
@@ -2,7 +2,29 @@
 	@php
     	$menus = session('admin_menu'); 
 	@endphp
-  
+	@foreach($menus as $k=>$v)
+    	@if($v['route']=='admin.index')
+         <li class="">
+         	<a class="home" href="javasctipt:" status=0>{{$v['name']}}</a>
+         	 <ul class="itemslist" style="display: none">
+         	 	<li><a class="" href="{{route($v['route'])}}">{{$v['name']}}</a></li>
+         	</ul>
+         </li>
+       	@else
+        <li class="">
+         	<a class="home" href="javasctipt:" status=0>{{$v['name']}}</a>
+         	 <ul class="itemslist" style="display: none">
+         	 	@if(isset($v['children']))
+         	 		@foreach($v['children'] as $kk=>$vv)
+         	 			<li><a href="{{route($vv['route'])}}">{{$vv['name']}}</a></li>
+         	 		@endforeach
+         	 	@else
+         	 		<li><a class="" href="{{route($v['route'])}}">{{$v['name']}}</a></li>
+         	 	@endif
+         	</ul>         	
+         </li>
+        @endif
+    @endforeach     
 </ul>    
 
 <script>
