eec37ceb15109ca24bd1d270794aff090191a0fa
chenhuojia <1126089253@qq.com>
2018-04-10 14:49:39 +0800

    modules

--- config/app.php
+++ config/app.php
@@ -176,6 +176,9 @@ return [
         // App\Providers\BroadcastServiceProvider::class,
         App\Providers\EventServiceProvider::class,
         App\Providers\RouteServiceProvider::class,
+	
+	//多modules
+	Caffeinated\Modules\ModulesServiceProvider::class,
 
     ],
 
@@ -225,7 +228,7 @@ return [
         'URL' => Illuminate\Support\Facades\URL::class,
         'Validator' => Illuminate\Support\Facades\Validator::class,
         'View' => Illuminate\Support\Facades\View::class,
-
+	'Module'=>Ceffeinated\Modules\Facades\Moudle::class,
     ],
 
 ];
