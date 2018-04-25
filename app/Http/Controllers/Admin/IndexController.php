49396b6277bd347fef9fc7f36d7533cba3513d9f
chenhuojia <1126089253@qq.com>
2018-04-25 11:26:35 +0800

    延时

--- app/Http/Controllers/Admin/IndexController.php
+++ app/Http/Controllers/Admin/IndexController.php
@@ -18,7 +18,7 @@ class IndexController extends Controller{
     public function indexTest(){
         
        $params=request()->all();
-       SendTest::dispatch($params)->delay(now()->addMinutes(10));
+       SendTest::dispatch($params)->delay(60);
        return $params;
     }
 }
